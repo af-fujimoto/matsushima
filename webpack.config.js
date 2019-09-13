5a1ca0e8f44852fbf6acf8aba846901b7ce95fc8
seiya.fujimoto <seiya.fujimoto@money-design.com>
2019-09-13 10:36:56 +0900

    add stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,54 @@
 const path = require('path');
-const webpack = require('webpack');
+const Webpack = require('webpack');
+const Autoprefixer = require('autoprefixer');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
 module.exports = {
     // モジュールバンドルを行う起点となるファイルの指定
     // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
     // 下記はオブジェクトとして指定した例
     entry: {
-        bundle: './src/app.ts'
+        bundle: './src/app.ts',
     },
     devtool: 'inline-source-map',
     output: {
         // モジュールバンドルを行った結果を出力する場所やファイル名の指定
         // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
-        path: path.join(__dirname,'dist'),
-        filename: '[name].js'  // [name]はentryで記述した名前(この例ではbundle）が入る
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js', // [name]はentryで記述した名前(この例ではbundle）が入る
     },
     // モジュールとして扱いたいファイルの拡張子を指定する
     // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
     // デフォルトは['.js', '.json']
     resolve: {
-        extensions:['.ts','.js']
+        extensions: ['.ts', '.js'],
     },
     devServer: {
         // webpack-dev-serverの公開フォルダ
-        contentBase: path.join(__dirname,'dist')
+        contentBase: path.join(__dirname, 'dist'),
     },
     // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
     module: {
         rules: [
 
             {
-                enforce: "pre",
+                enforce: 'pre',
                 test: /\.ts$/,
                 exclude: /node_modules/,
-                loader: "eslint-loader",
+                loader: 'eslint-loader',
                 options: {
-                    fix: true
-                }
+                    fix: true,
+                },
             },
             {
                 // 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
-                test:/\.ts$/,
+                test: /\.ts$/,
                 exclude: /node_modules/,
                 use: [
                     {
                         loader: 'ts-loader',
-                    }
-                ]
+                    },
+                ],
             },
             {
                 test: /\.scss$/,
@@ -57,45 +60,51 @@ module.exports = {
                         options: {
                             url: false,
                             sourceMap: true,
-                            importLoaders: 2
-                        }
+                            importLoaders: 2,
+                        },
                     },
                     {
                         loader: 'postcss-loader',
                         options: {
                             sourceMap: true,
                             plugins: [
-                                require('autoprefixer')({
-                                    grid: true
-                                })
-                            ]
-                        }
+                                Autoprefixer({
+                                    grid: true,
+                                }),
+                            ],
+                        },
                     },
                     {
                         loader: 'sass-loader',
                         options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
+                            sourceMap: true,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
                 use: {
-                    loader: "url-loader",
+                    loader: 'url-loader',
                     options: {
                         limit: 100 * 1024, // 100KB以上だったら埋め込まずファイルとして分離する
-                        name: './img/[name].[ext]'
-                    }
-                }
-            }
-        ]
+                        name: './img/[name].[ext]',
+                    },
+                },
+            },
+        ],
     },
     plugins: [
-        new webpack.ProvidePlugin({
+        new Webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        })
-    ]
-}
+            'window.jQuery': 'jquery',
+        }),
+        new StyleLintPlugin({
+            configFile: './.stylelintrc',
+            syntax: 'scss',
+            quiet: false,
+            fix: true,
+        }),
+    ],
+};
