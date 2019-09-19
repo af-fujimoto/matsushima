afba580344134e9b82c3f7d0c7384c190c9cb6b5
seiya.fujimoto <seiya.fujimoto@money-design.com>
2019-09-19 10:31:32 +0900

    code fix

--- src/js/base.ts
+++ src/js/base.ts
@@ -1,33 +1,33 @@
 type triggerFnc = (...args: any[]) => void;
 
 interface base {
-    listenTo: (target: base, triggerName: string, fnc: triggerFnc) => void;
-    trigger: (key: string, ...args: any[]) => void;
-    addTrigger: (key: string, fnc: triggerFnc) => void;
+  listenTo: (target: base, triggerName: string, fnc: triggerFnc) => void;
+  trigger: (key: string, ...args: any[]) => void;
+  addTrigger: (key: string, fnc: triggerFnc) => void;
 }
 
 export default abstract class Base implements base {
-    private _triggerList: {
-        [key: string]: triggerFnc | undefined;
-    };
+  private _triggerList: {
+      [key: string]: triggerFnc | undefined;
+  };
 
-    constructor() {
-        this._triggerList = {};
+  constructor() {
+    this._triggerList = {};
+  }
+
+  listenTo(target: base, triggerName: string, fnc: triggerFnc): void {
+    target.addTrigger(triggerName, fnc.bind(this));
+  }
+
+  trigger(key: string, ...args: any[]) {
+    const triggerFnc = this._triggerList[key];
+
+    if (triggerFnc) {
+      triggerFnc(...args);
     }
+  }
 
-    listenTo(target: base, triggerName: string, fnc: triggerFnc): void {
-        target.addTrigger(triggerName, fnc.bind(this));
-    }
-
-    trigger(key: string, ...args: any[]) {
-        const triggerFnc = this._triggerList[key];
-
-        if (triggerFnc) {
-            triggerFnc(...args);
-        }
-    }
-
-    addTrigger(key: string, fnc: triggerFnc) {
-        this._triggerList[key] = fnc;
-    }
+  addTrigger(key: string, fnc: triggerFnc) {
+    this._triggerList[key] = fnc;
+  }
 }
--- src/js/othello/stone.ts
+++ src/js/othello/stone.ts
@@ -1,31 +1,31 @@
 import Base from '../base';
 
 export default class Stone extends Base {
-    $el: JQuery<HTMLSpanElement>;
+  $el: JQuery<HTMLSpanElement>;
 
-    private _isBlack: boolean;
+  private _isBlack: boolean;
 
-    constructor($wrap: JQuery<HTMLLIElement>, color: stoneColor) {
-        super();
-        this.$el = $('<span class="stone" />');
-        this._isBlack = color === 'BLACK';
+  constructor($wrap: JQuery<HTMLLIElement>, color: stoneColor) {
+    super();
+    this.$el = $('<span class="stone" />');
+    this._isBlack = color === 'BLACK';
 
-        this.toggleStoneClass();
+    this.toggleStoneClass();
 
-        $wrap.append(this.$el);
-    }
+    $wrap.append(this.$el);
+  }
 
-    color(): stoneColor {
-        return this._isBlack ? 'BLACK' : 'WHITE';
-    }
+  color(): stoneColor {
+    return this._isBlack ? 'BLACK' : 'WHITE';
+  }
 
-    reverse() {
-        this._isBlack = !this._isBlack;
-        this.toggleStoneClass();
-    }
+  reverse() {
+    this._isBlack = !this._isBlack;
+    this.toggleStoneClass();
+  }
 
-    toggleStoneClass() {
-        this.$el.toggleClass('stone-black', this._isBlack);
-        this.$el.toggleClass('stone-white', !this._isBlack);
-    }
+  toggleStoneClass() {
+    this.$el.toggleClass('stone-black', this._isBlack);
+    this.$el.toggleClass('stone-white', !this._isBlack);
+  }
 }
